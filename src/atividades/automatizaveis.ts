2bcaaef308a8e1d8c5cf0157de70650b8abab6d3
annaccavalcanti <annacarolina7388@gmail.com>
2026-09-20 01:00:16 -0300

    Atualizando arquivos de automação e correções

---
+++ src/atividades/automatizaveis.ts
@@ -0,0 +1,24 @@
+import PromptSync from "prompt-sync";
+
+const prompt = PromptSync();
+
+let automatizaveis: number = 0;
+let naoAutomatizaveis: number = 0;
+
+for(let i = 1; i<=5; i++) {
+    const cenario: string = prompt(`Digite o ${i}º cenário: `);
+    const resposta: string = prompt(`É automatizável? (Sim/Não): `).toLowerCase();
+
+    if (resposta === "sim") {
+        console.log(`${cenario} -> Automatizar`);
+        automatizaveis++
+    } else if (resposta === "não"|| resposta =="nao") {
+        console.log(`${cenario} -> Não-Automatizar`);
+        naoAutomatizaveis++
+    } else{
+        console.log("Ocorreu um erro!");
+    }
+}
+
+console.log(`Total de cenários automatizaveis: ${automatizaveis}`);
+console.log(`Total de cenários não automatizaveis: ${naoAutomatizaveis}`);
